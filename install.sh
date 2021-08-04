0bbdf19e7a71ffc2ec40f2480bab6b807264db44
BuildTools <unconfigured@null.spigotmc.org>
2021-08-04 22:03:44 +0200

    Bugfix

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ cd "$basedir";
 echo >&2 "[SETUP] Installing package dependencies ...";
 npm install;
 
-# Compile source code
-echo >&2 "[SETUP] Compiling source code ...";
-./node_modules/.bin/tsc;
-
 # Copy the _config.ts file and rename it to config.ts if it does not exist
 if [ ! -f "config.ts" ] ; then
     echo >&2 "[SETUP] Config file not found! Copying ...";
     cp _config.ts config.ts;
 fi
+
+# Compile source code
+echo >&2 "[SETUP] Compiling source code ...";
+./node_modules/.bin/tsc;
 
 echo >&2 "[SETUP] Done!";
